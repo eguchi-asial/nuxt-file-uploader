49e01b958290d96eda3ab72e0b00f1794f9bed4d
悠江口 <yueguchi@yuueguchinoMacBook-Pro.local>
2019-08-17 13:06:02 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="container">
+    <div>
+      <h3 class="title">
+        失敗から知るAWSサーバレス <br />
+        <small>~ API Gateway Lambda編 ~</small>
+      </h3>
+      <p class="subtitle">これは失敗から知るAWSサーバレスの動作検証用です。</p>
+      <section class="upload-area">
+        <input type="file" name="uploadImage" @change="changeFile" />
+        <img :src="imageSrc" class="preview" />
+      </section>
+      <section class="result-area">
+        <p class="size">
+          raw image size: <strong>{{ rawImageSize }}</strong>
+        </p>
+        <p class="size">
+          Bae64 size: <strong>{{ base64Size }}</strong>
+        </p>
+        <div v-show="uploadTarget">
+          <button @click="postImage">
+            画像をAPI Gateway x Lambda APIにPOSTする
+          </button>
+        </div>
+        <textarea v-model="uploadTarget" class="result-base64" />
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      uploadTarget: '',
+      rawImageSize: '0 KB',
+      base64Size: '0 KB'
+    }
+  },
+  computed: {
+    imageSrc() {
+      return this.uploadTarget || '/images/NoImage.png'
+    }
+  },
+  methods: {
+    changeFile(data) {
+      const {
+        target: { files = [] }
+      } = data
+      if (!files.length) return
+      const file = files[0]
+      const reader = new FileReader()
+      this.rawImageSize = this.getAppropriateSize(file.size)
+      reader.readAsDataURL(file)
+      reader.onload = (evt) => {
+        const size = evt.target.result.length
+        this.base64Size = this.getAppropriateSize(size)
+        this.uploadTarget = evt.target.result
+      }
+    },
+    getAppropriateSize(size) {
+      return size > 0
+        ? size > 1048576
+          ? `${(size / 1048576).toFixed(1)} MB`
+          : `${(size / 1024).toFixed(1)} KB`
+        : 0
+    },
+    postImage() {
+      this.$axios.$post(`${process.env.BASE_URL}/image`)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+html {
+  font-size: 10px;
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
+      BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+      sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 5rem;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+
+  .subtitle {
+    font-weight: 300;
+    font-size: 2rem;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 1.5rem;
+  }
+
+  section {
+    margin: 2rem;
+    &.upload-area {
+      .preview {
+        width: 25rem;
+        height: 25rem;
+      }
+    }
+    &.result-area {
+      .size {
+        font-size: 1.8rem;
+        text-align: left;
+        strong {
+          color: red;
+        }
+      }
+    }
+  }
+
+  .result-base64 {
+    width: 100%;
+    height: 25rem;
+  }
+}
+</style>
